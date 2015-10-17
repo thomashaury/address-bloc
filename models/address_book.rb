f912495d5008592635482654b2cd82c6baa85cb6
thomas <tomhaury@gmail.com>
2015-10-16 17:29:38 -0700

    checkpoint 22 intro complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -21,7 +21,32 @@ class AddressBook
 # #11
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
-  
+
+  # Search AddressBook for a specific entry by name
+ def binary_search(name)
+   # #1
+    lower = 0
+    upper = entries.length - 1
+
+# #2
+    while lower <= upper
+# #3
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+# #4
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+   return nil
+ end
+
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
